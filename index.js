44f8a12312ab97ec4ad702c97fcd495adbf56d34
TaherAhmedAshraf <taheirahmedashraf@gmail.com>
2022-09-05 23:42:31 +0600

    express-router v1

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const fs = require('fs');
 const path = require('path');
-const supportedExtensions = ['js', 'ts'];
+const supportedExtensions = ['.js', '.ts'];
 
-
-function dynamicRouter(app, folder, ...middlewares) {
+function dynamicRouter({prefix='/', app, folder, middlewares=[], disableWarnings=false}) {
   fs.readdirSync(path.join(folder)).forEach(file => {
-    if(!supportedExtensions.includes(path.extname(file))) return
-
-    const route = require(path.join(folder, file));
-    app.use(route.path, ...middlewares, route);
+    // check if it is a directory
+    if(fs.lstatSync(path.join(folder, file)).isDirectory()) {
+      // if it is a directory, call the function again
+      dynamicRouter({
+        prefix: `${prefix}${file}/`,
+        app: app,
+        folder: path.join(folder, file),
+        middlewares: middlewares,
+        disableWarnings: disableWarnings
+      })
+    }else{
+      if(!supportedExtensions.includes(path.extname(file))) return
+      const route = require(path.join(folder, file));
+      if(route?.path) {
+        const router_path = route?.path() ?  `${prefix}${route.path().replace('/','')}` : `${prefix}${file.split('.')[0]}`
+        app.use(router_path, ...middlewares, route);
+      }else{
+        if(disableWarnings) return
+          console.warn(`Warning: ${file} is not a valid express router`);
+          console.warn(`Tips: if you want to ignore this warning, disableWarnings option in the config`);
+      }
+    }
 })};
 
 module.exports = dynamicRouter;
